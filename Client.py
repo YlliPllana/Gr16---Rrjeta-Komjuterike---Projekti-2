1c91b5f0ac331b2011f3531ef8161308c5c71203
RinaHalili <rina.halili@student.uni-pr.edu>
2024-11-09 19:27:13 +0100

    Update

--- Client.py
+++ Client.py
@@ -7,19 +7,33 @@ SERVER_PORT = 12345
 
 def start_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((SERVER_IP, SERVER_PORT))
-    print("Jeni lidhur me serverin.")
+
+    try:
+        client_socket.connect((SERVER_IP, SERVER_PORT))
+        print("Jeni lidhur me serverin.")
+    except ConnectionRefusedError:
+        print("Nuk u arrit lidhja me serverin. Ju lutemi kontrolloni që serveri është në punë.")
+        return
 
     try:
         while True:
 
-            request = input("Shkruani kerkesen ose 'EXIT' per te dale: ")
+            request = input("Shkruani kërkesën ose 'EXIT' për të dalë: ")
             if request.lower() == 'exit':
                 break
 
             client_socket.send(request.encode('utf-8'))
-            response = client_socket.recv(2048).decode('utf-8')
-            print("Pergjigja nga serveri:", response)
+
+
+            try:
+                response = client_socket.recv(2048).decode('utf-8')
+                print("Përgjigja nga serveri:", response)
+            except ConnectionAbortedError:
+                print("Serveri refuzoi kërkesën")
+                break
+            except socket.error as e:
+                print(f"Ndodhi një gabim gjatë leximit të përgjigjes: {e}")
+                break
     finally:
         client_socket.close()
         print("Klienti u shkëput.")
